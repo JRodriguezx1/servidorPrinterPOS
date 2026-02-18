179c99048c564dfed277f627ef7110c13b7f85ff
Julian Rodriguez <julianlink123@hotmail.com>
2026-02-18 01:46:13 -0500

    detectando la impresora

--- src/public/ts/index.ts
+++ src/public/ts/index.ts
@@ -3,6 +3,7 @@
 console.log(12);
 
 const selectPrinter = document.querySelector('#selectPrinter') as HTMLSelectElement;
+const formSelectPrinter = document.querySelector('#formSelectPrinter') as HTMLFormElement;
 
 interface printerPOS { Name:string, ShareName:string };
 
@@ -24,7 +25,15 @@ function printPrinterPOS(printers:printerPOS[]){
         console.log(z.ShareName);
         const op = document.createElement('option');
         op.textContent = z.ShareName;
+        op.value = z.ShareName;
         selectPrinter.append(op);
     });
 }
 
+
+formSelectPrinter.addEventListener('submit', (e:SubmitEvent)=>{
+    e.preventDefault();
+    console.log((e.submitter as HTMLButtonElement).id);
+    const selectPrinter = formSelectPrinter.elements.namedItem("selectPrinter") as HTMLSelectElement;
+    console.log(selectPrinter.value);
+});
